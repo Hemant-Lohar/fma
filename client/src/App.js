8db3829d2563e78cd9e2818ce497bfcfb765175e
Hemant-Lohar <hemantlohar585@gmail.com>
2022-12-15 00:06:04 +0530

    client updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,32 @@ import "./app.css";
 import axios from "axios";
 
 function App() {
-  const [name, setName] = useState("")
-  const [session, setSession] = useState("")
-  const [age, setAge] = useState(0)
-  const [gender, setGender] = useState("")
+  const [name, setName] = useState("");
+  const [session, setSession] = useState("");
+  const [age, setAge] = useState();
+  const [gender, setGender] = useState("");
 
-  const register = () => {
-    axios.post("http://localhost:3001/api/registration", {
-      name: name,
-      gender: gender,
-      age: age,
-      session: session,
-    }).then(() => {
-      alert("Registration Successfully !")
+  const [userdata, setuserdata] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fma-api.onrender.com/api/get").then((responce) => {
+      setuserdata(responce.data);
+      console.log(responce.data);
     });
+  }, []);
+
+  const register = async () => {
+    await axios
+      .post("https://fma-api.onrender.com/api/registration", {
+        name: name,
+        gender: gender,
+        age: age,
+        session: session,
+      })
+      .then(alert("Registration Successfully !")).then(
+        setName(""),
+        setAge()
+      );
   };
 
   return (
@@ -30,6 +42,7 @@ function App() {
             type="text"
             name="name"
             id="name"
+            value={name}
             onChange={(e) => {
               setName(e.target.value);
             }}
@@ -37,14 +50,11 @@ function App() {
         </div>
         <div className="input">
           <label htmlFor="gender">Gender</label>
-          <input
-            type="text"
-            name="gender"
-            id="gneder"
-            onChange={(e) => {
-              setGender(e.target.value);
-            }}
-          />
+          <select onChange={(e) => setGender(e.target.value)}>
+            <option>Gender</option>
+            <option value={"Male"}>Male</option>
+            <option value={"Female"}>Female</option>
+          </select>
         </div>
         <div className="input">
           <label htmlFor="age">Age</label>
@@ -52,6 +62,7 @@ function App() {
             type="number"
             name="age"
             id="age"
+            value={age}
             onChange={(e) => {
               setAge(e.target.value);
             }}
@@ -59,18 +70,39 @@ function App() {
         </div>
         <div className="input">
           <label htmlFor="session">session</label>
-          <input
-            type="text"
+          <select
             name="session"
             id="session"
             onChange={(e) => {
               setSession(e.target.value);
             }}
-          />
+          >
+            <option value={"6-7 AM"}>6-7 AM</option>
+            <option value={"7-8 AM"}>7-8 AM</option>
+            <option value={"8-9 AM"}>8-9 AM</option>
+            <option value={"5-6 AM"}>5-6 PM</option>
+          
+          </select>
+          
         </div>
 
-        <button onClick={register}>Submit</button>
+        <p>Monthly fee: 500/- Rs</p>
+
+        <button onClick={register}>Register</button>
       </div>
+
+      {/* <div className="list">
+        <h3>data</h3>
+        {userdata.map((item, key) => {
+          return (
+            <>
+              <p key={item.name}>
+                name: {item.name} | age: {item.age}
+              </p>
+            </>
+          );
+        })}
+      </div> */}
     </div>
   );
 }
