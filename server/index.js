ee562b100f47a60eb55ffebe8bfdf86cdd003705
Hemant-Lohar <hemantlohar585@gmail.com>
2022-12-15 00:07:04 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const db = mysql.createPool({
   host: "remotemysql.com",
@@ -11,24 +11,30 @@ const db = mysql.createPool({
   database: "tohhqARa3H",
 });
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/api/registration", (req, res) => {
+  const name = req.body.name;
+  const gender = req.body.gender;
+  const age = req.body.age;
+  const session = req.body.session;
 
-  const name = req.body.name
-  const gender = req.body.gender
-  const age = req.body.age
-  const session = req.body.session
-    
   const sqlinsert =
     "INSERT INTO fma (name,gender,age, session) VALUE (?,?,?,?);";
   db.query(sqlinsert, [name, gender, age, session], (err, result) => {
-    console.log(result);
+    res.send()
   });
 });
 
-app.listen(3001, () => {
+app.get("/api/get", (req, res) => {
+  const sqlselect = "SELECT * FROM fma;";
+  db.query(sqlselect, (err, result) => {
+    res.send(result)
+  });
+});
+
+app.listen("3001", () => {
   console.log(`running`);
 });
